f6a19af96f81d47350af811ea32ea70bac3e3326
Roman Korchnev <romo4ko.mail@gmail.com>
2024-07-23 22:56:40 +0300

    added company page

--- resources/js/views/HomePage.vue
+++ resources/js/views/HomePage.vue
@@ -1,7 +1,18 @@
 <template>
   <div class="row">
     <div class="col-md-5 mx-auto">
-      Компонент <b>views/HomePage.vue</b>
+      <main class="text-center mt-5">
+
+        <p class="fw-bold">ultrasource</p>
+
+        <div class="">
+          <p>Наименование юридического лица: ООО "Ультра Сорс"</p>
+          <p>Адрес: 346880, Ростовская область, г. Батайск, ул. Половинко, д. 280/10, к. 2, кв. 162</p>
+          <p>Телефон: +79064286128</p>
+          <p>ИНН: 6141057868</p>
+        </div>
+
+      </main>
     </div>
   </div>
 </template>
